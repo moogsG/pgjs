3c90d807b99a0dc6bfaf93ef8f64ec0365bc3713
Morgan Greff <morgan@greffs.com>
2017-09-26 20:16:35 +0000

    Cleaned up strings, removed returns

--- view_people.js
+++ view_people.js
@@ -1,25 +1,27 @@
 const knex = require('./knex_connect');
 let values = process.argv.slice(2).toString();
+
+/*Calls Knex select and where
+*Sends matching value to print funtion
+*/
 knex('famous_people').select()
   .where('last_name', '=', values)
   .orWhere('first_name', '=', values)
   .then((list) => {
     console.log('Searching...');
     print(list);
-
-    return knex.destroy();
+    knex.destroy();
   })
   .catch((err) => {
     console.error(err);
-    return knex.destroy();
+    knex.destroy();
   });
 
+/*Prints results
+*/
 let print = (list) => {
-  console.log('Found ' + list.length + ' person(s) by the name ' + values + ':')
-  for(let person of list){
-    console.log(person.id + ': ' +
-                person.first_name + ' ' +
-                person.last_name + ', born "' +
-                person.birthdate.toISOString().substr(0, 10) + '"');
+  console.log(`Found ${list.length} person(s) by the name ${values}:`)
+  for (let person of list) {
+    console.log(`${person.id}: ${person.first_name} ${person.last_name}, born "${person.birthdate.toISOString().substr(0, 10)}"`);
   }
 }
