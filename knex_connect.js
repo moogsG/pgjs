115f3d2281367fc53a615a61304a81058c5b476b
Morgan Greff <morgan@greffs.com>
2017-09-26 21:13:33 +0000

    Added knexfile to knex_connect

--- knex_connect.js
+++ knex_connect.js
@@ -1,3 +1,2 @@
 const settings = require("./settings"); // settings.json
-
-module.exports = require('knex')(require('./knexfile')[process.env.NODE_ENV]);
+module.exports = require('knex')(require('./knexfile')['development']);
